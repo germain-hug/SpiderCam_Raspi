3213914cc91af57d205d0522c34264bbc347c197
Hugo Germain <hugo.germain@supelec.fr>
2017-07-05 16:33:41 +0200

    Fixed speed issue

--- arduino_sketch/ax-12w_driver/ax-12w_driver.ino
+++ arduino_sketch/ax-12w_driver/ax-12w_driver.ino
@@ -18,7 +18,8 @@ void callback( const std_msgs::Float32& vel){
       // vel : Velocity Value (must be between 0 and 1023)
       // dir : 0 for Clockwise | 1 for CounterClockwise
       int dir = 0;
-      ax12SetRegister2(1, AX_GOAL_SPEED_L, (int(vel.data)&0x03FF) | (dir<<10));
+      ax12SetRegister2(1, AX_GOAL_SPEED_L, (int(vel.data)&0x03FF) | (dir<<10));    delay(33);
+
 }
 
 ros::Subscriber<std_msgs::Float32> sub("cmd_vel_approved", &callback );
