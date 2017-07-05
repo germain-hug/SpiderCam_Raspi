5478966c17cc3b8efc3e6762287643112da373df
Hugo Germain <hugo.germain@supelec.fr>
2017-07-05 15:44:29 +0100

    Update startup_script.sh

--- startup_script.sh
+++ startup_script.sh
@@ -2,10 +2,15 @@
 echo "Exporting ROS_MASTER_URI"
 export ROS_MASTER_URI=http://hugo-MacBookPro:11311/
 
-#echo "Sourcing ROS"
-#source /opt/ros/kinetic/setup.bash
-#source /home/pi/catkin_ws/devel/setup.bash
-
 echo "Git Pull from SpiderCam_Raspi"
 cd /home/pi/catkin_ws/src && git pull origin master
 
+echo "Uploading Arduino Sketch"
+cd /home/pi/catkin_ws/src/arduino_sketch && arduino --upload ax-12w_driver/ax-12w_driver.ino
+
+echo "Starting Rosserial"
+rosrun rosserial_python serial_node.py /dev/ttyUSB0 _baud:=1000000
+sleep 15
+
+echo "Starting Safety Node"
+rosrun cmd_vel_controller safety_check.py
