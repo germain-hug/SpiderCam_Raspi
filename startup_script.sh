3d17e883ceef4195ae865f1eb7db524ed9f83c0c
Hugo Germain <hugo.germain@supelec.fr>
2017-07-06 15:48:27 +0100

    Update startup_script.sh

--- startup_script.sh
+++ startup_script.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
-echo "---- Exporting ROS_MASTER_URI ----"
-echo " "
+
+#echo "---- Exporting ROS_MASTER_URI ----"
 #export ROS_MASTER_URI=http://hugo-MacBookPro:11311/
 export ROS_MASTER_URI=http://172.20.10.2:11311
 export ROS_HOSTNAME=172.20.10.2
@@ -8,25 +8,25 @@ export ROS_HOSTNAME=172.20.10.2
 echo  "---------------------------------------"
 echo  "---- Git Pull from SpiderCam_Raspi ----"
 echo  "---------------------------------------"
-
+echo " "
 cd /home/pi/catkin_ws/src && git pull origin master
-
+echo " "
 echo "----------------------------------"
 echo "---- Uploading Arduino Sketch ----"
 echo "----------------------------------"
-
+echo " "
 cd /home/pi/catkin_ws/src/arduino_sketch && arduino --upload ax-12w_driver/ax-12w_driver.ino
-
-
+echo " "
 echo "------------------------------"
 echo "---- Starting Safety Node ----"
 echo "------------------------------"
-
+echo " "
 rosrun cmd_vel_controller safety_check.py &
 sleep 5
-
+echo " "
 echo "----------------------------"
 echo "---- Starting Rosserial ----"
 echo "----------------------------"
+echo " "
 rosrun rosserial_python serial_node.py /dev/ttyUSB0 _baud:=1000000
 sleep 15
