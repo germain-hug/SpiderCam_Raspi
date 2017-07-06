01ac0eda134682a597525968da287850055c18e8
Hugo Germain <hugo.germain@supelec.fr>
2017-07-06 10:04:19 +0100

    Update safety_check.py

--- cmd_vel_controller/scripts/safety_check.py
+++ cmd_vel_controller/scripts/safety_check.py
@@ -12,35 +12,35 @@ transmits velocity to cmd_vel_controller node
 """
 
 def callback(data, args):
-	
 	this_vel = 0
 	
-	pub = args[0]
-	self_ID = args[1]
-
-	# Retrieve 
-	if(self_ID==1):
+	# Retrieve corresponding velocity value
+	if(args[1]==1):
 		this_vel = data.vel_1
-	elif(self_ID==2):
+	elif(args[1]==2):
 		this_vel = data.vel_2
-	elif(self_ID==3):
+	elif(args[1]==3):
 		this_vel = data.vel_3
-	elif(self_ID==4):
+	elif(args[1]==4):
 		this_vel = data.vel_4
-
-  	# TODO : SAFETY CHECKS (on position as well ?)	
-	print(this_vel)
-	pub.publish(this_vel)
+	
+	# Publish velocity
+	args[0].publish(this_vel)
 
 def cmd_vel_publisher(self_ID):
+	
+	# Define publishing topic according to Motor ID
+	topic_out = 'cmd_vel_approved_' + self_ID;
 	rospy.init_node('safety_check', anonymous=True)
-	pub = rospy.Publisher('cmd_vel_approved', Float32, queue_size=10)
+	pub = rospy.Publisher(topic_out, Float32, queue_size=10)
+	
+	# Subscribe to the master commands
     	rospy.Subscriber('cmd_vel_out', cmd_vel_motors, callback, (pub, self_ID))
     	rospy.spin()
 
 if __name__ == '__main__':
-	with open('/home/pi/catkin_ws/specs.json') as data_file:    
+	with open('/home/pi/catkin_ws/specs.json') as data_file: 
+		# Read Motor ID from JSON file
     		data = json.load(data_file)
 		self_ID = data["ID"]
-		print("self_ID", self_ID)
 	cmd_vel_publisher(self_ID)
