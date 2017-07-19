418c48d4e5f58d2df29b7e9709118f6b614ba77b
Hugo Germain <hugo.germain@supelec.fr>
2017-07-19 14:42:14 +0100

    Update startup_script.sh

--- startup_script.sh
+++ startup_script.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 
-#source /opt/ros/kinetic/setup.bash
-#source /home/pi/catkin_ws/devel/setup.bash
-#export PATH=$PATH:/opt/arduino-1.8.1/
+PATH=/sbin:/usr/sbin:/bin:/usr/bin:/usr/local/bin:/home/pi/catkin_ws/src
+source /opt/ros/kinetic/setup.bash
+source /home/pi/catkin_ws/devel/setup.bash
+export PATH=$PATH:/opt/arduino-1.8.1/
 
 cd /home/pi/catkin_ws/src/
 export ROS_MASTER_URI=http://172.20.10.2:11311 # Master IP
